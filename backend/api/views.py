76faa3881393f26ddc5b8602e4eb98bc38dae434
NikolayN-ru <napadaylonikolay@gmail.com>
2022-04-05 22:03:35 +0600

    5/3.04.22

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, permissions
 from .models import NoteBook
 from .serializers import NoteBookSerializer
 
 
-class NoteBookAPIView(generics.ListAPIView):
+class NoteBookAPIView(generics.ListCreateAPIView):
 	queryset = NoteBook.objects.all()
 	serializer_class = NoteBookSerializer
 	
-class DetailNoteBookAPIView(generics.RetrieveAPIView):
+class DetailNoteBookAPIView(generics.RetrieveUpdateDestroyAPIView):
+	permission_classes = (permissions.IsAdminUser,)
 	queryset = NoteBook.objects.all()
 	serializer_class = NoteBookSerializer
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -39,19 +39,25 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
-        
+    'corsheaders',
+
     'api',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ORIGIN_WHITELIST = (
+    'http://localhost:3000',
+    'http://localhost:8000', )
 
 ROOT_URLCONF = 'backend.urls'
 
